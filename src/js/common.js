8072bb842d5029b3a3d44f2ab651a6765f1e121a
HorbynkoIvan <horbynko.i@gmail.com>
2020-12-21 09:15:07 +0200

    snow

--- src/js/common.js
+++ src/js/common.js
@@ -4,12 +4,14 @@ import './../scss/common.scss';
 
 import $ from 'jquery';
 import 'slick-carousel';
+import 'jquery-snowfall';
 
 window.$ = window.jQuery = $;
 
 window.addEventListener('DOMContentLoaded', () => {
     openMore();
-
+   // $(".showcase").snowfall({image :"/assets/img/flake.png", minSize: 10, maxSize:32});
+    $(".showcase").snowfall();
     function openMore() {
         const $btnMore = $('#btn_more');
         const $shadow = $('.about_top .shadow');
