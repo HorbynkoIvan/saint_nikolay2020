9bd673eb47b0fbcc403c581f7cccd377d6ab6ba5
HorbynkoIvan <horbynko.i@gmail.com>
2021-05-21 14:59:01 +0300

    first commit

--- src/js/common.js
+++ src/js/common.js
@@ -9,34 +9,7 @@ import 'jquery-snowfall';
 window.$ = window.jQuery = $;
 
 window.addEventListener('DOMContentLoaded', () => {
-    openMore();
-    $(".saint_nikolay2020").snowfall({
-        image: "https://cdn.parfums.ua/public/upload/newAdminUpload/Landing/NYvichy89/flake1.png",
-        minSize: 10,
-        maxSize: 32
-    });
-    // $(".saint_nikolay2020").snowfall({
-    //     minSize: 5,
-    //     maxSize: 10,
-    //     round: true,
-    //     shadow: true,
-    //     flakeCount: 100,
-    //     maxSpeed: 5
-    // });
-    function openMore() {
-        const $btnMore = $('#btn_more');
-        const $shadow = $('.about_top .shadow');
-        const $aboutBottom = $('.about_bottom');
 
-        $btnMore.on('click', function () {
-            $(this).find('span').text(function (i, text) {
-                return text === "Читати повністю" ? "Згорнути" : "Читати повністю";
-            })
-
-            $aboutBottom.slideToggle();
-            $shadow.fadeToggle()
-        })
-    }
 
     $('.banner .inner').click(function(){
         window.location.href='https://parfums.ua/ua/promotions/dostavka-krasoti';
